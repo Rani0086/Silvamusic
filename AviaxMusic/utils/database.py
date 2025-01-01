3ddef2811236c71fe1f0edaf2f8aed2c857f6414
Marwin2002 <musicbotid@gmail.com>
2025-01-02 02:24:51 +0530

    Update database.py

--- AviaxMusic/utils/database.py
+++ AviaxMusic/utils/database.py
@@ -226,7 +226,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from VIPMUSIC.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -240,7 +240,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from VIPMUSIC.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -267,7 +267,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from VIPMUSIC.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -280,7 +280,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from VIPMUSIC.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
