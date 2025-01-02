34d64379b1ce42810a5e29c5566a6f1d2267f8e4
Marwin2002 <musicbotid@gmail.com>
2025-01-02 11:46:57 +0530

    Update settings.py

--- AviaxMusic/plugins/bot/settings.py
+++ AviaxMusic/plugins/bot/settings.py
@@ -49,15 +49,6 @@ async def settings_mar(client, message: Message, _):
         reply_markup=InlineKeyboardMarkup(buttons),
     )
 
-@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
-@languageCB
-async def gib_repo(client, CallbackQuery, _):
-    await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://envs.sh/5DB.mp4"),
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
-        ),
-    )
     
 @app.on_callback_query(filters.regex("settings_helper") & ~BANNED_USERS)
 @languageCB
