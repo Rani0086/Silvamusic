6f478a08ab15895c087b858a2d10dc4d780937aa
Marwin2002 <musicbotid@gmail.com>
2025-01-02 15:16:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MONGO_DB_URI = "mongodb+srv://tennyson2002:tennyson2002@cluster0.h1vm3.mongodb.n
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 6000))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002250226716))
 
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = "7224419362"
